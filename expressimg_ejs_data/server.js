f28d262ec54e3ce1cccf54b79f61788858e01d08
RomellPineda <romell.pineda@gmail.com>
2018-07-05 22:07:18 -0700

    added expressimg.js and express_img_data.js

---
+++ expressimg_ejs_data/server.js
@@ -0,0 +1,75 @@
+// Load the express module and store it in the variable express (Where do you think this comes from?)
+var express = require("express");
+// invoke express and store the result in the variable app
+var app = express();
+// use app's get method and pass it the base route '/' and a callback
+app.get('/', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.send("<h1>Express Cars and Cats Data operational</h1>");
+})
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+    console.log(__dirname + "////////////")
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+// app.get("/cars", function (request, response){
+//     var these_cars = [
+//         {photo: "/images/car1.jpg"}, 
+//         {photo: "/images/car2.jpg"}, 
+//         {photo: "/images/car3.jpeg"}, 
+//         {photo: "/images/car4.jpg"}, 
+//     ];
+//     console.log(these_cars.photo);
+//     response.render('cars', {cars: these_cars});
+// })
+app.get("/cats", function (request, response){
+    var these_cats = [
+        {photo: "/images/cat1.png"}, 
+        {photo: "/images/cat2.jpg"}, 
+        {photo: "/images/cat3.jpeg"}, 
+        {photo: "/images/cat4.jpg"}, 
+    ];
+    response.render('cats', {cats: these_cats});
+})
+
+// INFO INFO INFO
+app.get("/images/cat1.png/info", function (request, response){
+    var info = [
+        {photo: "/images/cat1.png", name: "Tigger", food: "honey", age: "?", sleeps: ["anywhere"]}
+    ];
+    console.log(info);
+    response.render('details', {info: info});
+})
+app.get("/images/cat2.jpg/info", function (request, response){
+    var info = [
+        {photo: "/images/cat2.jpg", name: "Nia", food: "gazelles", age: "3", sleeps: ["ground", "trees"]}
+    ];
+    console.log(info);
+    response.render('details', {info: info});
+})
+app.get("/images/cat3.jpeg/info", function (request, response){
+    var info = [
+        {photo: "/images/cat3.jpeg", name: "Bob", food: "gazelles", age: "5", sleeps: ["ground", "trees",]}
+    ];
+    console.log(info);
+    response.render('details', {info: info});
+})
+app.get("/images/cat4.jpg/info", function (request, response){
+    var info = [
+        {photo: "/images/cat4.jpg", name: "Katt", food: "tacos", age: "46", sleeps: ["bed", "couch", "ground", "trees"]}
+    ];
+    console.log(info);
+    response.render('details', {info: info});
+})
+
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
--- expressimgejs/server.js
+++ expressimgejs/server.js
@@ -1,16 +1,14 @@
 // Load the express module and store it in the variable express (Where do you think this comes from?)
 var express = require("express");
-console.log("Let's find out what express is", express);
 // invoke express and store the result in the variable app
 var app = express();
-console.log("Let's find out what app is", app);
 // use app's get method and pass it the base route '/' and a callback
 app.get('/', function(request, response) {
     // just for fun, take a look at the request and response objects
    console.log("The request object", request);
    console.log("The response object", response);
    // use the response object's .send() method to respond with an h1
-   response.send("<h1>Yo Express!</h1>");
+   response.send("<h1>Express Cars and Cats operational</h1>");
 })
 // this is the line that tells our server to use the "/static" folder for static content
 app.use(express.static(__dirname + "/static"));
@@ -21,15 +19,24 @@ app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views'); 
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
 app.set('view engine', 'ejs');
-app.get("/users", function (request, response){
-    // hard-coded user data
-    var users_array = [
-        {name: "Michael", email: "michael@codingdojo.com"}, 
-        {name: "Jay", email: "jay@codingdojo.com"}, 
-        {name: "Brendan", email: "brendan@codingdojo.com"}, 
-        {name: "Andrew", email: "andrew@codingdojo.com"}
+app.get("/cars", function (request, response){
+    var these_cars = [
+        {photo: "/images/car1.jpg"}, 
+        {photo: "/images/car2.jpg"}, 
+        {photo: "/images/car3.jpeg"}, 
+        {photo: "/images/car4.jpg"}, 
     ];
-    response.render('shmusers', {users: users_array});
+    console.log(these_cars.photo);
+    response.render('cars', {cars: these_cars});
+})
+app.get("/cats", function (request, response){
+    var these_cats = [
+        {photo: "/images/cat1.png"}, 
+        {photo: "/images/cat2.jpg"}, 
+        {photo: "/images/cat3.jpeg"}, 
+        {photo: "/images/cat4.jpg"}, 
+    ];
+    response.render('cats', {cats: these_cats});
 })
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
