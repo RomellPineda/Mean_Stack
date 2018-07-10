598d0d65a3a16c692456bc08e009435fa1f1f1ee
RomellPineda <romell.pineda@gmail.com>
2018-07-10 13:58:20 -0700

    added socket_survey

--- socket_survey/server.js
+++ socket_survey/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + "/public"));
 const server = app.listen(1337);
 const io = require('socket.io')(server);
-var counter = 0;
 
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
@@ -15,13 +16,10 @@ app.get("/", function (req, res) {
 });
 io.on("connection", function (socket) { //2
 
-    socket.emit("greeting", { msg: "Hello this is the Server transmission" }); //3
-    socket.on("thankyou", function (data) { //7
-        console.log(data.msg); //8 (note: this log will be on your server's terminal)
-    });
-    socket.on("posting_form", function(survey_data){
-        console.log(survey_data);
+    socket.on("posting_form", function(object){
+        console.log(object);
         console.log("Server says: Did I get the survey data?");
-        socket.emit("updated_message", {msg: survey_data});
+        socket.emit("updated_message", {response : "You emitted the following information to the server:", survey_datar : object});
+        socket.emit("random_number", {response : "Your lucky number emitted by the server is:", random : Math.ceil(Math.random()*1000)});
     });
 });
