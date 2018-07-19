021f52481bdd3744acbea0205f84abacd6fbdd21
RomellPineda <romell.pineda@gmail.com>
2018-07-19 15:42:44 -0700

    modified restful_task_api

--- restful_task_api/public/src/app/app.component.ts
+++ restful_task_api/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,49 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Mean';
+  num: number;
+  randNum: number;
+  str: string;
+  first_name: string;
+  snacks: string[];
+  loggedIn: boolean;
+  tasks = [];
+  missions: any;
+
+  // onButtonClickParam(num: Number): void { 
+  //   console.log(`Click event is working with num param: ${num}`);
+  //   // call the service's method to post the data, but make sure the data is bundled up in an object!
+  //   let observable = this._httpService.postToServer({data: num});
+  //   observable.subscribe(data => console.log("Got our data!", data));
+  // }
+
+  onButtonClick(): void { 
+    console.log(`Click event triggered`);
+    // call the service's method to post the data, but make sure the data is bundled up in an object!
+    let observable = this._httpService.getTasks();
+    // observable.subscribe(data => console.log("Acquired Tasks!", data));
+    observable.subscribe(data =>{console.log('Acquired Tasks', data)
+    this.missions = data;
+    })
+  }
+
   constructor(private _httpService: HttpService){}
+  ngOnInit(){
+    this.num = 911;
+    this.randNum = Math.floor( (Math.random()  * 2 ) + 1);
+    this.str = 'Hello Angular Developer!';
+    this.first_name = 'Alpha';
+    this.snacks = ["vanilla latte with skim milk", "brushed suede", "cookie"];
+    this.loggedIn = true;
+    this.getTasksFromService()
+  }
+  getTasksFromService(){
+    let taskObservable = this._httpService.getTasks();
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    taskObservable.subscribe(data => {console.log("Got the data on initial start up!", data)
+    this.tasks = data['tasks'];
+    })
+  }
 }
--- restful_task_api/public/src/app/http.service.ts
+++ restful_task_api/public/src/app/http.service.ts
@@ -6,12 +6,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
   constructor(private _http: HttpClient) {
-    this.getTasks();
+    console.log('Run and Gun');
+    // this.getTasks();
   }
   getTasks(){
     // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
+    // let tempObservable = this._http.get('https://pokeapi.co/api/v2/pokemon/13/');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    console.log('Service getTasks triggered');
+    return this._http.get('/tasks');
+    // let obs = this._http.get('/tasks');
+    // obs.subscribe(data=>{console.log(data)});
+    // return obs;
  }
+ postToServer(num){
+  // use the .post() method of HttpClient
+  // num must be an object
+  // provide the url of your post route - make sure this is set up in your server!
+  return this._http.post('/tasks', num);  
+  }
 }
